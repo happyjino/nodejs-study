5efdab169c913a0e3124b5d2c90fa30b11c76986
happyjino <wlsgh1128@naver.com>
2022-08-31 10:34:19 +0900

    template 기능 추가
    template 기능을 추가했다. url에서 id값을 받아 사이트를 띄운다.

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
     console.log('1: ', queryData.id);
     if(_url == '/'){
-      _url = '/index.html';
+      // _url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
         response.writeHead(404);
@@ -15,7 +17,26 @@ var app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    console.log('2: ', __dirname + _url);
-    response.end(queryData.id);
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=nodejs1">nodejs1</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p>
+        준비.txt를 보고 세팅한다!<br>
+        response.end() 안의 내용에 따라 결과가 달라진다.
+      </p>
+    </body>
+    </html>
+    `;
+    response.end(template);
 });
 app.listen(3000);
