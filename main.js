9dba5f7ac37b2d2fcf635d4bc8446f7ddb01834c
happyjino <wlsgh1128@naver.com>
2022-08-31 10:25:36 +0900

    url 사용하기
    url을 사용하도록 수정했다

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log('1: ', queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
- 
+    console.log('2: ', __dirname + _url);
+    response.end(queryData.id);
 });
 app.listen(3000);
