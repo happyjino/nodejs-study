5645873159d82698397f1ea9711d21d333201d5d
happyjino <wlsgh1128@naver.com>
2022-08-31 13:28:34 +0900

    함수로 정리
    templateHTML, LIST를 이용해서 중복되는 부분을 정리해줬다.

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body){
+  return `
+            <!doctype html>
+            <html>
+            <head>
+              <title>${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              ${body}
+            </body>
+            </html>
+          `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i<filelist.length){
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i += 1;
+  }
+  list += '</ul>'
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -15,57 +42,17 @@ var app = http.createServer(function(request,response){
           }
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = '<ul>';
-          var i = 0;
-          while(i<filelist.length){
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            i += 1;
-          }
-          list += '</ul>'
-          var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-                ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         });
       } else{
         fs.readdir('./data', function(error, filelist){
+          var list = templateList(filelist);
           fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
             var title = queryData.id;
-            var list = '<ul>';
-            var i = 0;
-            while(i<filelist.length){
-              list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-              i += 1;
-            }
-            list += '</ul>'
-            var template = `
-              <!doctype html>
-              <html>
-              <head>
-                <title>${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                  ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-              </body>
-              </html>
-            `;
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
             response.writeHead(200);
             response.end(template);
           });
