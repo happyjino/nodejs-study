1ce6d2193fd087ed9dcdad731f9d01863a359174
happyjino <wlsgh1128@naver.com>
2022-08-31 10:57:06 +0900

    404 에러 추가
    없는 경로로 갔을 경우 수정을 해준다.
    코드에 약간의 수정을 했다. 불필요한 부분이 제거됐다.

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const path = require('path');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=nodejs1">nodejs1</a></li>
+              <li><a href="/?id=nodejs2">nodejs2</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+    }else{
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
-      var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=nodejs1">nodejs1</a></li>
-            <li><a href="/?id=nodejs2">nodejs2</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-      `;
-      response.end(template);
-    })
 });
 app.listen(3000);
