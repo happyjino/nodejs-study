446c0eb615206f65a2c4a6821a128eaaee856055
happyjino <wlsgh1128@naver.com>
2022-08-31 11:03:40 +0900

    pathname 기능 추가
    pathname 기능 추가해서 깔끔하게 정리했다! 맘에 든다!

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-const path = require('path');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/'){
-      fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
+      if(queryData.id === undefined){
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
         var template = `
           <!doctype html>
           <html>
@@ -31,7 +31,31 @@ var app = http.createServer(function(request,response){
         `;
         response.writeHead(200);
         response.end(template);
-      })
+      } else{
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err,description){
+          var title = queryData.id;
+          var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=nodejs1">nodejs1</a></li>
+                <li><a href="/?id=nodejs2">nodejs2</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+      }
     }else{
       response.writeHead(404);
       response.end('Not found');
