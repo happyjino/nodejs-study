e06a8effb635d3949f0c6c26d976bcaaaf83aa2f
happyjino <wlsgh1128@naver.com>
2022-09-01 14:26:01 +0900

    삭제 기능 추가
    fs.unlink를 통해 파일 삭제기능을 추가했습니다.

--- main.js
+++ main.js
@@ -58,7 +58,13 @@ var app = http.createServer(function(request,response){
             var title = queryData.id;
             var template = templateHTML(title, list,
               `<h2>${title}</h2><p>${description}</p>`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+              `
+                <a href="/create">create</a> <a href="/update?id=${title}">update</a>
+                <form action="/delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>
+              `);
             response.writeHead(200);
             response.end(template);
           });
@@ -146,6 +152,20 @@ var app = http.createServer(function(request,response){
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end()
           })
+        })
+      });
+    }else if(pathname === '/delete_process'){
+      var body = ''
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(error){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`./data/${id}`, function(error){
+          if(error){console.log(error);}
+          response.writeHead(302, {Location: '/'});
+          response.end()
         })
       });
     }else{
