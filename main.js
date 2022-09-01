9a1e19b78859f5e86ddd5921ac53ec6575c6c28b
happyjino <wlsgh1128@naver.com>
2022-09-01 11:30:59 +0900

    글 생성 기능 추가1
    create 버튼을 만들고 글 생성 기능을 추가하는 중이다

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
             <body>
               <h1><a href="/">WEB</a></h1>
               ${list}
+              <a href="/create">create</a>
               ${body}
             </body>
             </html>
@@ -58,6 +59,29 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        if(error){
+          console.log(error);
+        }
+        var title = 'create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="http://localhost:3000/process_create" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      });
+    }else if(pathname='/process_create'){
+      
     }else{
       response.writeHead(404);
       response.end('Not found');
