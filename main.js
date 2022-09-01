3d14827915136a2f8339e38c1e3aecbcb7783410
happyjino <wlsgh1128@naver.com>
2022-09-01 13:45:25 +0900

    글쓰기 기능 추가2
    글쓰기 폼에서 받은 정보를 처리해서 글쓰기를 완료했다!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -81,7 +82,23 @@ var app = http.createServer(function(request,response){
         response.end(template);
       });
     }else if(pathname='/process_create'){
-      
+      var body ='';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, function(error){
+          if(error){
+            console.log(error)
+          }
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end()
+        })
+      });
+      response.writeHead(200);
     }else{
       response.writeHead(404);
       response.end('Not found');
