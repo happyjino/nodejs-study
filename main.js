96d19b1cbb03b9410e4cf0860bac6c2239e7e481
happyjino <wlsgh1128@naver.com>
2022-09-01 15:11:27 +0900

    template 모듈화
    template를 모듈로 만들어서 main.js를 정리했다!

--- main.js
+++ main.js
@@ -2,36 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-var template = {
-  HTML:function templateHTML(title, list, body, control){
-    return `
-              <!doctype html>
-              <html>
-              <head>
-                <title>${title}</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                ${control}
-                ${body}
-              </body>
-              </html>
-            `;
-  }, 
-  list:function templateList(filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i<filelist.length){
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-      i += 1;
-    }
-    list += '</ul>'
-    return list;
-  }
-}
+var template = require('./lib/template');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
